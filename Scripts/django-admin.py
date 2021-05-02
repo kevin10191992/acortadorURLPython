2dc63af98b3582fe69ed80a2f927c06653ed3752
kperez <kevin10191992@hotmail.com>
2021-05-02 09:50:55 -0500

    init

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\kperez\Desktop\Ejemplos\pyton\acortador_url\entorno\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
